0139ceeb96ae491b8131acc77b677aaaf1ca6406
Grzegorz Kastelik <grzegorz.kastelik@euvic.pl>
2020-10-05 16:45:39 +0200

    use image component

--- resources/js/components/layouts/logo/Logo.jsx
+++ resources/js/components/layouts/logo/Logo.jsx
@@ -1,16 +1,17 @@
 import React, { useContext } from 'react';
 import './Logo.scss';
 import { ServerDataContext } from '../../../context';
+import { Image } from 'antd';
 
 export default function Logo(props) {
   const { routes } = useContext(ServerDataContext);
 
   return (
-    <a href={routes.main}>
-      <img
+    <a href={routes.main} className="main-logo">
+      <Image
+        preview={false}
         src={props.dark ? '/images/logo.svg' : '/images/logo_white.svg'}
         alt="Logo EduPlace.me"
-        className="main-logo px-3"
       />
     </a>
   );
